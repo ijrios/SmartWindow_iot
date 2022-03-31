ba0ca6e7b964e1848bb5addfbc290bc40231d879
Jose Rios <joserios055@gmail.com>
2022-03-31 17:04:39 -0500

    This is a new commit for what I originally planned to be amended

--- controlVentana.ino
+++ controlVentana.ino
@@ -3,10 +3,10 @@ boolean lastState = false;
 int enablePin = 11; // PWM 
 int in1Pin = 10; // Control del motor
 int in2Pin = 9; // Control del motor
-int switchPin = 7; // Boton de encendido - se cambiará por una fotoresistencia
+int switchPin = 7; // Boton de encendido 
 int switchPin2 = 2; // Fin de carrera ventana abierta
 int switchPin3 = 3; // Fin de carrera ventana cerrada
-int pinLDR = 0; // Pin analogico de entrada para el LDR
+int pinLDR = A0; // Pin analogico de entrada para el LDR
 int valorLDR = 0; // Variable donde se almacena el valor del LDR
 int ledpin = 12; // Led puerta abierta
 int ledpin2 = 13; // Led puerta cerrada
@@ -24,6 +24,8 @@ void setup()
   pinMode(switchPin3, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(switchPin2), blink, FALLING);
   attachInterrupt(digitalPinToInterrupt(switchPin3), blink, FALLING);
+  Serial.begin(9600); //Inicializamos monitor serie para visualizar los valores de LDR. 
+  
 }
 
 void loop()
@@ -31,9 +33,9 @@ void loop()
   int estado =0;
   int speed = 255; // Velocidad
   valorLDR= analogRead(pinLDR); // Guardamos el valor leido del ADC en una variable
-  Serial.println(valorLDR); // Imprimimos el valor leido 
+  Serial.println(valorLDR);      //Imprimimos dicho valor, comprendido entre 0 y 1023. 
  //-------------------------------------------------------------------------------------
-  // Caso 1 - Botón
+   // Caso 1 - Botón
   if(digitalRead(switchPin) == LOW && digitalRead(switchPin) != lastState) // mirando para ver si el estado del botón es LOW y no es igual al último estado.
 { 
   reverse = !reverse; // solo cambiará cuando el estado del botón sea BAJO
@@ -44,14 +46,14 @@ void loop()
    // Caso 2 - Sensor LDR con poca luz
  if(valorLDR > 256)
   {
-  reverse = !reverse; // solo cambiará cuando el estado del botón sea BAJO
-  lastState = digitalRead(switchPin);
-  setMotor(speed, reverse);
-  digitalWrite(ledpin,HIGH);
+  //reverse = !reverse; // solo cambiará cuando el estado del botón sea BAJO
+  //lastState = digitalRead(switchPin);
+  //setMotor(speed, reverse);
+  //digitalWrite(ledpin,HIGH);
   }
   //-------------------------------------------------------------------------------------
    // Caso 3 - Sensor LDR con mucha luz
-  if(valorLDR < 256)
+  if(valorLDR < 256 && analogRead(valorLDR) != lastState)
   {
   reverse = !reverse; // solo cambiará cuando el estado del botón sea BAJO
   lastState = digitalRead(switchPin);
@@ -62,7 +64,6 @@ void loop()
    // Caso 4 - sensor de lluvia
    
   //-------------------------------------------------------------------------------------
-
 }
 
  //---------------------------------------------------------------------------------------
